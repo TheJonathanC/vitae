ca1205ad6e99e13ae953fcb09674f92fd837d2a3
jon vps <jonathan.s.correa45@gmail.com>
2026-09-23 17:21:00 +0000

    fix(ci): fix MSI bundle failure for pre-release versions and add platform icons

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,10 @@
 fn main() {
+    #[cfg(target_os = "linux")]
+    {
+        println!("cargo:rustc-link-search=native=/home/jon/.local/lib");
+        println!("cargo:rustc-link-arg=-Wl,-rpath,$ORIGIN");
+        println!("cargo:rustc-link-arg=-Wl,-rpath,$ORIGIN/../lib");
+        println!("cargo:rustc-link-arg=-Wl,-rpath,/home/jon/.local/lib");
+    }
     tauri_build::build()
 }
